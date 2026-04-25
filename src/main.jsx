9c6c7fad8ce296a023e646128b75aeea313294cb
marioblades-cmyk <marioblades@gmail.com>
2026-04-25 18:41:36 -0400

    fix: globally disable number input changes on scroll wheel

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Deshabilitar el cambio de valores con el scroll en inputs numéricos de forma global
+document.addEventListener('wheel', (e) => {
+    if (document.activeElement && document.activeElement.type === 'number') {
+        e.preventDefault();
+    }
+}, { passive: false });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
